a8e887dc1ad927570d7138eaa72d2c393298ffe8
ria vadhavkar <vadhavkar.ria@gmail.com>
2026-01-30 11:16:47 -0500

    scraped dinos - stored images in the public repo and the data in the library

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen items-center justify-center size-full py-8 px-8 pb-16 gap-8">
-
-    </div>
-  );
-}
